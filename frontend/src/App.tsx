bedcf6e59b66b2aaf966b47d1a97772684f2704a
siddarththiru <siddarththiru@gmail.com>
2026-04-08 14:08:27 +0100

    Removed components and API client; refactor App entry point and global styles as part of refactoring

--- frontend/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AgentBuilderPage from "./pages/AgentBuilder";
-
-const App: React.FC = () => {
-  return <AgentBuilderPage />;
-};
-
-export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom/client";
 import "./styles/global.css";
+import { App } from "./app/App";
 
-const container = document.getElementById("root");
-if (container) {
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
